b222b3705e9e28bece4e540d8a8d7524422b5d70
jerinrajjsss <jerinr960@gmail.com>
2024-05-20 20:48:17 +0530

    implement delect confirm or cancel account dialog with complect functions and ui

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -12,7 +12,7 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-
+import { useConfirm } from "@/hooks/use-confirm";
 import {
   Table,
   TableBody,
@@ -39,6 +39,12 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "you are about to preform a bulk delete."
+  )
+
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -65,6 +71,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog/>
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -81,9 +88,14 @@ export function DataTable<TData, TValue>({
                  size={'sm'}
                  variant={'destructive'}
                  className="ml-auto font-normal text-xs"
-                 onClick={() =>{
-                  onDelete(table.getFilteredSelectedRowModel().rows)
-                  table.resetRowSelection();
+                 onClick={async () =>{
+
+                  const ok = await confirm();
+                 
+                  if(ok){
+                    onDelete(table.getFilteredSelectedRowModel().rows)
+                    table.resetRowSelection();
+                  }
                  }}
                 >
                 <Trash className="size-4 mr-2" />
