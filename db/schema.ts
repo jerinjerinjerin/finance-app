d6dd47e16227149e113a772e628d580a4347881d
jerinrajjsss <jerinr960@gmail.com>
2024-05-22 21:29:51 +0530

    implement bulk-create transactions end poitn as well as complect transactions hooks

--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 import { createInsertSchema } from "drizzle-zod";
 import { relations } from "drizzle-orm";
-import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { 
+  integer, 
+  pgTable, 
+  text, 
+  timestamp
+ } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
