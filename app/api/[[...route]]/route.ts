ebc455b9e7ab35e45323089989240cb896f0f136
jerinrajjsss <jerinr960@gmail.com>
2024-05-22 17:11:25 +0530

    implement complete transactions api end point

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 
+import transactions from './transactions'
 import accounts from './accounts';
 import categories from './categories';
 
@@ -15,6 +16,7 @@ const app = new Hono().basePath('/api');
 const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app);
 export const POST = handle(app);
