4599478cf5d131786a85ec3dd190798619e3e160
jerinrajjsss <jerinr960@gmail.com>
2024-05-29 20:40:33 +0530

    implement bulk-create-transactions-successfully-csv-file-import-data-based

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -1,8 +1,9 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
+import { format, parse } from 'date-fns';
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { ImportTable } from "./import-table";
+import { convertAmountToMiliunits } from "@/lib/utils";
 
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
@@ -30,24 +31,65 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
     columnIndex: number,
     value: string | null
   ) => {
-    setSelectedColumns((prev) =>{
-        const newSelectedColumns = {...prev};
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
 
-        for (const key in newSelectedColumns){
-            if(newSelectedColumns[key] === value){
-                newSelectedColumns[key] = null;
-            }
-
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
         }
+      }
 
-        if(value === "skip"){
-            value = null;
-        }
+      if (value === "skip") {
+        value = null;
+      }
 
-        newSelectedColumns[`column_${columnIndex}`] = value;
-        return newSelectedColumns;
-    })
+      newSelectedColumns[`column_${columnIndex}`] = value;
+      return newSelectedColumns;
+    });
+  };
+
+  const progress = Object.values(selectedColumns).filter(Boolean).length;
+
+  const handleContuine = () => {
+    const getColemIndex = (column: string) => {
+
+      return column.split("_")[1];
   }
+
+  const mappedData = {
+    headers: headers.map((_header, index) => {
+      const columIndex = getColemIndex(`column_${index}`)
+      return selectedColumns[`column_${columIndex}`] || null;
+    }),
+
+    body:body.map((row) => {
+      const transFormedRow = row.map((cell, index) => {
+        const columnIndex = getColemIndex(`column_${index}`);
+        return selectedColumns[`column_${columnIndex}`] ? cell : null;
+      });
+
+      return transFormedRow.every((item) => item === null)
+      ? [] : transFormedRow;
+    }).filter((row) => row.length > 0),
+  }
+  const arrayData = mappedData.body.map((row) => {
+    return row.reduce((acc: any, cell, index) => {
+      const header = mappedData.headers[index];
+      if(header != null) {
+        acc[header] = cell;
+      }
+      return acc;
+    },{})
+  })
+  const formattedData = arrayData.map((item) =>({
+    ...item,
+    amount: convertAmountToMiliunits(parseFloat(item.amount)),
+    date: format((item.date, dateFormat, new Date()), outputFormat)
+  }))
+  //console.log({ formattedData });
+  onSubmit(formattedData);
+}
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -59,9 +101,23 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
           <CardTitle className="text-xl line-clamp-1">
             Import Transactions
           </CardTitle>
-          <div className="flex items-center gap-x-2">
-            <Button size={"sm"} onClick={onCancel}>
+          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
+            <Button 
+              size={"sm"} 
+              onClick={onCancel}
+              className="w-full lg:w-auto"
+            >
               Cancel
+            </Button>
+            <Button
+               size={"sm"}
+               disabled={
+                  progress < requireOptins.length 
+               }
+               onClick={handleContuine}
+               className="w-full lg:w-auto"
+            >
+              Continue ({progress} / {requireOptins.length})
             </Button>
           </div>
         </CardHeader>
